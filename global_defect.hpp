76ccfa646204fc8b2dda2553b3d9b295bea7a85d
maulikthebhatt <maulikdbhatt1996@gmail.com>
2021-06-05 01:01:30 +0200

    global file revert

--- global_defect.hpp
+++ global_defect.hpp
@@ -25,7 +25,6 @@ using namespace LATfield2;
 #include "metadata.hpp"
 #include "parser.hpp"
 #include "background.hpp"
-#include "powerSpectra.hpp"
 //#include "read_text.hpp"
 
 //#include <dir.h>
@@ -43,23 +42,19 @@ private:
 	metadata * sim_;
 	defects_metadata * defects_sim_;
 */
-	Field<float> phi_defect_;
-	Field<float> pi_defect_;
-	Field<float> pi_defect_prev_;
-
-	Field<float> T00_defect_;
+	Field<double> phi_defect_;
+	Field<double> pi_defect_;
+	Field<double> pi_defect_prev_;
+	Field<double> T00_defect_;
 	Field<double>Tii_defect_;
-
-	Field<Imag> T00_defect_k_;
-	PlanFFT<Imag> planT00_;
-
-	double lambda;
+	Field<double>Potential_;
+	
 	int firststep;
 
 public:
 	void initialize(Lattice * lat, Lattice * klat, double *dx, metadata * sim, defects_metadata * defects_sim);
 	void initialize(double *dx);
-	void generate_init_cond(string h5filename, string filename_phi, string filename_pi);
+	void generate_init_cond(string filename_phi, string filename_pi, string h5filename);
 	void update_phi(double *dt);
 	void update_pi(double *dt, double *a, double *adot_overa);
 	void writedefectSnapshots(string h5filename,const int snapcount);
@@ -68,12 +63,12 @@ public:
 	void write_Tuv_defect(string h5filename, const int snapcount);
 
 	unsigned long int random_seed();
-	double potential(Site & x);
 	double potentialprime(Site & x, int comp);
 	double modsqphi(Site & x);
 	double averagephi();
 	double averagerhodefect();
-	void compute_defect_pk_(string h5filename, const int count);
+	double potential(Site & x);
+	void test_global_defect(string h5filename, const int snapcount, double z, double dtau, double tau);
 
 
 };
@@ -105,11 +100,9 @@ void GlobalDefect::initialize(Lattice * lat, Lattice * klat, double *dx, metadat
 	Tuv_defect_.initialize(*lat_,4,4,symmetric);
 	Tuv_defect_.alloc();
 	
-	T00_defect_k_.initialize(*klat_);
-	T00_defect_k_.alloc();
-
-	planT00_.initialize(&T00_defect_,&T00_defect_k_);
-
+	Potential_.initialize(*lat_);
+	Potential_.alloc();
+	
 	firststep = 0;
 }
 
@@ -122,52 +115,100 @@ unsigned long int GlobalDefect::random_seed()
 }
 
 
-void GlobalDefect::generate_init_cond(string h5filename, string filename_phi = "", string filename_pi = "")
+void GlobalDefect::generate_init_cond(string filename_phi, string filename_pi, string h5filename)
 {
-	if(filename_phi == "" && filename_pi == "")
+	Site X(phi_defect_.lattice());
+
+	int i;
+	int numpoints = 0;
+	FILE * phifile;
+	FILE * pifile;
+
+	char linephi[MAX_LINESIZE];
+	char linepi[MAX_LINESIZE];
+	double dummy1, dummy2, dummy3, dummy4;
+	double *phi0, *phi1, *pi0, *pi1;
+	
+	linephi[MAX_LINESIZE-1] = 0;
+	linepi[MAX_LINESIZE-1] = 0;
+
+	std::string filestring = string("test/") + string("phi") + to_string(1) + string("_test10.txt");
+	phifile = fopen(filestring.c_str(), "r");
+
+	while (!feof(phifile) && !ferror(phifile))
 	{
-		Site x(phi_defect_.lattice());
-		const gsl_rng_type * T;
-		gsl_rng * r;
+		fgets(linephi, MAX_LINESIZE, phifile);
+		if (sscanf(linephi, " %lf %lf", &dummy1, &dummy2) == 2 && !feof(phifile) && !ferror(phifile)) numpoints++;
+	}
 
-		gsl_rng_env_setup();
+	phi0 = (double *) malloc(sizeof(double) * numpoints);
+	phi1 = (double *) malloc(sizeof(double) * numpoints);
+	pi0 = (double *) malloc(sizeof(double) * numpoints);
+	pi1 = (double *) malloc(sizeof(double) * numpoints);
 
-		gsl_rng_default_seed = random_seed();
+	COUT << numpoints << endl;
+	fclose(phifile);
 
-		T = gsl_rng_default;
-		r = gsl_rng_alloc (T);
+//	if(parallel.rank() == 0)
+//	{
 
-		for(x.first();x.test();x.next())
+	for(int z=0; z<sim_->numpts; z++)
+	{
+		int qr = 0;
+		int i = 0;
+
+		std::string filestring_phi = string("test/") + string("phi") + to_string(z) + string("_test10.txt");
+		phifile = fopen(filestring_phi.c_str(), "r");
+
+		std::string filestring_pi = string("test/") + string("pi") + to_string(z) + string("_test10.txt");
+//		COUT << filestring_pi << endl;
+		pifile = fopen(filestring_pi.c_str(), "r");
+
+		while (!feof(phifile) && !ferror(phifile) && !feof(pifile) && !ferror(pifile))
 		{
-			double phiNorm2 = 0;
-			for(int c = 0; c < defects_sim_->nComponents; c++)
+			fgets(linephi, MAX_LINESIZE, phifile);
+			fgets(linepi, MAX_LINESIZE, pifile);
+
+			if (sscanf(linephi, " %lf %lf", &dummy1, &dummy2) == 2 && !feof(phifile) && !ferror(phifile) && sscanf(linepi, " %lf %lf", &dummy3, &dummy4) == 2 && !feof(pifile) && !ferror(pifile))
 			{
-				phi_defect_(x,c) = gsl_ran_gaussian (r,1);
-				phiNorm2 += phi_defect_(x,c)*phi_defect_(x,c);
-			}
-			double ratio =  sqrt(defects_sim_->eta2/phiNorm2);
-			for(int c = 0; c < defects_sim_->nComponents; c++)
-			{
-				phi_defect_(x,c) *= ratio;
-				pi_defect_(x,c) = 0;
+				phi0[i] = dummy1 ;
+				phi1[i] = dummy2 ;
+				pi0[i] = dummy3 ;
+				pi1[i] = dummy4 ;
+				i++;
 			}
 		}
-		gsl_rng_free (r);
-		COUT << "Initial values of phi and pi are loaded!" << endl;
-	}
-	else if(filename_phi != "" && filename_pi != "")
-	{
-		phi_defect_.loadHDF5(filename_phi);
-		pi_defect_.loadHDF5(filename_pi);
-		COUT << "loading data done!" << endl;
-	}
 
-#ifdef EXTERNAL_IO
-	COUT << "Currently defect snapshot does not work with external IO" << endl;
-#else
-	phi_defect_.saveHDF5(h5filename + "phi_defect_init.h5");
-	pi_defect_.saveHDF5(h5filename + "pi_defect_init.h5");
-#endif
+		for(int y=0; y<sim_->numpts; y++)
+		{
+			for(int x=0; x<sim_->numpts; x++)
+			{
+				qr++;
+				if(X.setCoord(x, y, z))
+				{
+					phi_defect_(X,0) = phi0[qr];
+					phi_defect_(X,1) = phi1[qr] ;
+					pi_defect_(X,0) = pi0[qr] ;
+					pi_defect_(X,1) = pi1[qr] ;
+				}
+//				COUT << phi_defect_(x,y,z,0) << " " << phi_defect_(x,y,z,1) << " " << z << " " << qr << endl;
+			}
+		}
+
+//		COUT << qr << " ";
+		fclose(phifile);
+		fclose(pifile);
+	}
+//	}
+
+	COUT << "loading data done!" << endl;
+
+//#ifdef EXTERNAL_IO
+//	COUT << "Currently defect snapshot does not work with external IO" << endl;
+//#else
+//	phi_defect_.saveHDF5(h5filename + "phi_defect_init.h5");
+//	pi_defect_.saveHDF5(h5filename + "pi_defect_init.h5");
+//#endif
 
 	COUT << COLORTEXT_MAGENTA << "Initial Condition for defect is generated" << COLORTEXT_RESET << endl << endl;
 }
@@ -184,6 +225,7 @@ void GlobalDefect::update_phi(double *dt)
 		phi_defect_(x,c) += *dt_ * pi_defect_(x,c);
 	}
 	}
+
 	phi_defect_.updateHalo(); 
 }
 
@@ -193,7 +235,7 @@ double GlobalDefect::potentialprime(Site & x,
 {
 	double phiNorm2 = 0;
 	for(int i =0; i < defects_sim_->nComponents; i++)phiNorm2 += phi_defect_(x,i)*phi_defect_(x,i);
-	return 2.0 * lambda * ( phiNorm2 - defects_sim_->eta2) *  phi_defect_(x,comp);
+	return 2.0 * defects_sim_->lambda * ( phiNorm2 - defects_sim_->eta2) *  phi_defect_(x,comp);
 }
 
 
@@ -205,7 +247,7 @@ void GlobalDefect::update_pi(double *dt,
 	double *a_ = a;
 	double *adot_overa_ = adot_overa;
 	double friction_coefficient = 1;
-	lambda = defects_sim_->lambda0/ *a / *a;
+
 	Site x(pi_defect_.lattice()); 
 
 	if(defects_sim_->dissipation)
@@ -222,7 +264,7 @@ void GlobalDefect::update_pi(double *dt,
 	double c2 = *dt_ / (1.0 + *dt_ * (*adot_overa_));
 	double a2 = *a_ * *a_;
 
-	float * temp = pi_defect_prev_.data_;
+	double * temp = pi_defect_prev_.data_;
 	pi_defect_prev_.data_ = pi_defect_.data_;
 	pi_defect_.data_ = temp;
 
@@ -236,6 +278,7 @@ void GlobalDefect::update_pi(double *dt,
 		pi_defect_(x,c) = c1 * pi_defect_prev_(x,c) + c2 * ( lapPhi -  a2 * potentialprime(x,c) );
 	}
 	}
+
 }
 
 
@@ -243,7 +286,7 @@ double GlobalDefect::potential(Site & x)
 {
 	double phiNorm2 = 0;
 	for(int i =0; i < defects_sim_->nComponents; i++) phiNorm2 += phi_defect_(x,i) * phi_defect_(x,i);
-	return lambda * ( phiNorm2 - defects_sim_->eta2) * ( phiNorm2 - defects_sim_->eta2) / 2.0;
+	return defects_sim_->lambda * ( phiNorm2 - defects_sim_->eta2) * ( phiNorm2 - defects_sim_->eta2) / 2.0;
 }
 
 void GlobalDefect::compute_Tuv_defect(double a)
@@ -265,7 +308,6 @@ void GlobalDefect::compute_Tuv_defect(double a)
 		{
 			temp = pi_defect_(x,c);
 			mpidot += temp*temp;
-			
 		}
 		else
 		{
@@ -276,15 +318,18 @@ void GlobalDefect::compute_Tuv_defect(double a)
 		for(int i = 0;i<3;i++)
 		{
 			gradPhi[i] = ( phi_defect_(x+i,c) - phi_defect_(x-i,c) ) / 2.0 / *dx_ ; // / sim_->boxsize; 
-			temp = ( phi_defect_(x+i,c) - phi_defect_(x-i,c) ) / 2.0 / *dx_ ;
+			temp = ( phi_defect_(x+i,c) - phi_defect_(x-i,c) ) / 2.0 / *dx_  / sim_->boxsize;
 			gradPhi2_ += temp*temp;
 		}
 		}
 
 		gradPhi2 = (gradPhi[0]*gradPhi[0])+(gradPhi[1]*gradPhi[1])+(gradPhi[2]*gradPhi[2]);
 
-		T00_defect_(x) = a2 * (mpidot / 2.0 / a2   + potential(x) + gradPhi2_ / 2.0 / a2) ;
-		Tuv_defect_(x, 0, 0) = a2 * (mpidot / 2.0 / a2 + potential(x) + gradPhi2 / 2.0 / a2) ;
+		T00_defect_(x) = mpidot / 2.0 / a2 + potential(x) + gradPhi2_ / 2.0 / a2;
+
+		if (T00_defect_(x) >3000 ) COUT << "mpidot / 2.0 / a2 = " << mpidot / 2.0 / a2 << " AND potential = " << potential(x) << " AND gradPhi2_ / 2.0 / a2 = " << gradPhi2_ / 2.0 / a2 << "                        T00 defect = " << T00_defect_(x) << endl;
+
+		Tuv_defect_(x, 0, 0) = mpidot / 2.0 / a2 + potential(x) + gradPhi2 / 2.0 / a2;
 
 		Tuv_defect_(x, 1, 1) = mpidot / 2.0 - potential(x)* a2 - gradPhi2 / 2.0 + gradPhi[0] * gradPhi[0];
 		Tuv_defect_(x, 2, 2) = mpidot / 2.0 - potential(x)* a2 - gradPhi2 / 2.0 + gradPhi[1] * gradPhi[1];
@@ -378,19 +423,52 @@ double GlobalDefect::averagerhodefect()
 	return rhoavg_;
 }
 
-void GlobalDefect::compute_defect_pk_(string h5filename, const int count)
+void GlobalDefect::test_global_defect(string h5filename,const int snapcount,double z,double dtau,double tau)
 {
-	planT00_.execute(FFT_FORWARD); 
-	char filename_def[2*PARAM_MAX_LENGTH+24];
-	sprintf(filename_def, "%03d", count);
+	Site x(phi_defect_.lattice());
+	for(x.first();x.test();x.next())
+	{
+		Potential_(x) = potential(x);
+	}
 
-	output_powerSpectrum(T00_defect_k_,
-                          h5filename + filename_def + "defect_pk.txt",
-                          sim_->numbins,
-                          1.0,
-                          false,
-                          false,
-                          true,
-                          false);
+	char filename_def[2*PARAM_MAX_LENGTH+24];
+	sprintf(filename_def, "%05d", snapcount);
+
+#ifdef EXTERNAL_IO
+	COUT << "Currently defect snapshot does not work with external IO" << endl;
+#else
+	Potential_.saveHDF5(h5filename + filename_def + "_potential_global_.h5");
+	Potential_.saveHDF5(h5filename + filename_def + "_potential_global_.h5");
+#endif
+
+
+//	if(snapcount ==0)
+//	{
+//		ofstream globaldefectfile;
+//		if(parallel.rank() == 0)
+//		{
+//			
+//			
+//			globaldefectfile.open (h5filename + "global_defect_test.txt",std::ios_base::app);
+//			globaldefectfile << z << " " << dtau << " " << tau << " " << snapcount << endl;
+//			globaldefectfile.close();
+//		}
+//	}
+//	else
+//	{
+	ofstream globaldefectfile;
+	if(parallel.rank() == 0)
+	{
+		globaldefectfile.open (h5filename + "global_defect_test.txt",std::ios_base::app);
+		globaldefectfile << "#z" << " " << "#dt" << " " << "#tau" << " " << "#snapcount"<< endl;
+		globaldefectfile.close();
+		
+		globaldefectfile.open (h5filename + "straight_defect.txt",std::ios_base::app);
+		globaldefectfile << z << " " << dtau << " " << tau << " " << snapcount << endl;
+		globaldefectfile.close();
+	}
+//	}
+
 }
+
 #endif
